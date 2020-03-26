c95c7fca3b0f3da1fb6da0c20de1b8339a615912
grumpyting <yantingyeo584@gmail.com>
2020-03-26 17:56:46 +0100

    js extern, css
    klok kleiner, margin raket minder

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-      const secondHand = document.querySelector('.second-hand');
+const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+var rocket = document.querySelector('rocket');
 
 function setDate() {
   const now = new Date();
@@ -8,7 +9,8 @@ function setDate() {
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
+/*  rocket.style.transform =`rotate(${secondsDegrees}deg)`;
+*/
   const mins = now.getMinutes();
   const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
@@ -16,8 +18,20 @@ function setDate() {
   const hour = now.getHours();
   const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
 }
 
 setInterval(setDate, 1000);
 
 setDate();
+
+var current= new Date()
+  var day_night=current.getHours()
+  if (day_night<18){ 
+    document.getElementById("welcome").innerHTML = "Good day!";
+    document.querySelector('.inner-clock-face').style.backgroundImage= 'url("earth.png")';
+
+  }else{
+    document.getElementById("welcome").innerHTML = "Good evening!";
+    document.querySelector('.inner-clock-face').style.backgroundImage = 'url("night.png")';
+  }
