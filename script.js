737937ce133bf0e5d03727e318acb13f4ca9393a
grumpyting <yantingyeo584@gmail.com>
2021-03-17 18:33:59 +0100

    comments addedd

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
-var rocket = document.querySelector('rocket');
 
 function setDate() {
   const now = new Date();
@@ -17,7 +16,7 @@ function setDate() {
   const hour = now.getHours();
   const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-
+/*Hierboven worden de seconden omgerekend naar juiste format en vervolgens in graden van een cirkel omgezet*/
 }
 
 setInterval(setDate, 1000);
@@ -34,6 +33,7 @@ var current= new Date()
     document.getElementById("welcome").innerHTML = "Good evening!";
     document.querySelector('.inner-clock-face').style.backgroundImage = 'url("night.png")';
   }
+/*wanneer het eerder is dan 18 uur dan laat hij de image zien "earth.png" zo niet is het avond en laat hij night.png zien.*/
 
 var today = new Date()
 document.getElementById('fullDate').innerHTML = today.getDate() + '/' + (today.getMonth()+1) +"/"+today.getFullYear();
